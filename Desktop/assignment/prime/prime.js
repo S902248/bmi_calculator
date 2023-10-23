b0be8f8ccac9d3b93071cb1e388acca58ea5da48
shubhamadhav007 <adhavshubham628@gmail.com>
2023-10-23 19:22:56 +0530

    added prime no code

---
+++ Desktop/assignment/prime/prime.js
@@ -0,0 +1,12 @@
+let number=0;
+for (let i=1;i<=number;i++){
+    if (number%i==0){
+        count++;
+    }
+}
+if (count==2){
+    console.log(number,"Prime Number");
+}
+else{
+    console.log(number,"is not a Prime Number");
+}
