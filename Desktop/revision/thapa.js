633b8753377e428eb99661731db64d72b7bc268c
S902248 <alibabapanti@gmail.com>
2024-01-18 15:59:56 +0530

    a

---
+++ Desktop/revision/thapa.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Thapa wala nepal </h1>,
+          document.getElementById("root"));
+
+          ReactDOM.render(React.creatElement("h1",
+          null,
+          "Thapa technical"),
+          document.getElementById("root"));
