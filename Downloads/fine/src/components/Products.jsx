403b8acd401b1904a3bceadb01b926cfaa4da510
S902248 <alibabapanti@gmail.com>
2024-03-12 21:07:54 +0530

    add

---
+++ Downloads/fine/src/components/Products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  // TODO: Remove below const and instead import them from chakra
+  const Flex = () => <div />;
+  const Grid = () => <div />;
+
+  return (
+    <Flex>
+      <Grid>{/* List of Products */}</Grid>
+      {/* Pagination */}
+    </Flex>
+  );
+};
+
+export default Products;
